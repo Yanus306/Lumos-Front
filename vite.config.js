1a8cb395e75cdf9736566f9a704b57327a0995a5
Radian06 <aksen05aksen@gmail.com>
2026-02-11 13:51:50 +0900

    feat: policy 페이지 구현

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  // 개발 서버 설정
-  server: {
-    port: 5173,
-    open: '/src/popup/popup.html', // npm run dev 하면 바로 팝업창을 띄워줌
-  },
+  root: 'src', 
   build: {
+    outDir: '../dist',
     rollupOptions: {
       input: {
-        // 팝업 페이지 경로를 지정해줍니다.
         popup: resolve(__dirname, 'src/popup/popup.html'),
-        // 나중에 팀원들이 줄 content.js나 background.js도 여기에 추가할 예정입니다.
+        policy: resolve(__dirname, 'src/pages/index.html'), 
       },
     },
   },
+
+  server: {
+    open: true,
+  }
 });
